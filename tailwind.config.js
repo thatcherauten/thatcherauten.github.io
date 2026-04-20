9f89583f63efd00c11bae463ec4eb21d3bb26a7b
thatcherauten <thatcherauten@ksu.edu>
2026-04-20 00:18:13 -0500

    still trying to fix darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
-
-// tailwind.config.js
 module.exports = {
-    // ...
+    darkMode: 'class',
+    content: [
+        './components/**/*.{vue,js}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './app.vue',
+    ],
     plugins: [require('tailwindcss-primeui')]
 };
